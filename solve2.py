6975572dfa3e69e7a7f101b1a26c0ad86014ed5e
matthewyu0202-oss <matthew.yu0202@gmail.com>
2026-03-25 12:54:16 +0900

    solve2.py
    과제 HW-1

--- solve2.py
+++ solve2.py
@@ -3,5 +3,17 @@ def solve2(format_str):
     answer = 0
     
     # 여기에 코드를 작성하십시오.
+    a=format_str[0]
+    if a=='N':
+        answer=10
+    elif a=='C':
+        answer=26
     
+    b={'N':10,'C':26}
+    for i in range(1,len(format_str)):
+        c=b.get(format_str[i])
+        if format_str[i-1]==format_str[i]:
+            answer=answer*(c-1)
+        elif format_str[i-1]!=format_str[i]:
+            answer=answer*c
     return answer
